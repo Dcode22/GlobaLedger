5a64ce0f46d1d9717ad2847ef9994ce7320123ea
Dcode22 <68872584+Dcode22@users.noreply.github.com>
2024-07-28 23:47:17 +0300

    set views path

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(session({
 
 app.use(express.urlencoded({extended: true})) // to parse urlencoded data
 app.use(express.json()) // to parse json data
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
 app.use((req, res, next) => {
